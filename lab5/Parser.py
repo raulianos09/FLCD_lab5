4d61b625518118d1db76149a9957db786cdf72b5
raulianos09 <raul_ianos@yahoo.com>
2021-12-07 10:03:42 +0200

    parsing table

--- lab5/Parser.py
+++ lab5/Parser.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 
+
 class Production:
     def __init__(self, lhs, rhs):
         self.rhs = rhs
@@ -14,12 +15,12 @@ class Production:
     def __eq__(self, other):
         return self.rhs == other.rhs and self.lhs == other.lhs
 
+
 class Parser:
     def __init__(self, grammar):
         self.grammar = grammar
         self.productions = []
         self.augment_grammar()
-
 
     def augment_grammar(self):
         self.grammar.addProduct("S\'", "S")
@@ -29,18 +30,17 @@ class Parser:
         self.grammar.setInitialNonterminal("S\'")
 
     def col_can_LR0(self):
-        s0 = self.closure(Production("S\'", [".","S"]))
+        s0 = self.closure(Production("S\'", [".", "S"]))
         C = [s0]
         copy_of_C = []
         while copy_of_C != C:
             copy_of_C = deepcopy(C)
             for state in C:
                 for x in self.grammar.getAlphabet():
-                    gt = self.goTo(state,x)
+                    gt = self.goTo(state, x)
                     if gt != [] and gt not in C:
                         C.append(gt)
         return C
-
 
     def closure(self, item):
         closure = [item]
@@ -51,7 +51,7 @@ class Parser:
                 condition, nonterminal = self.dot_before_nonterminal(production)
                 if condition is True:
                     for p in self.grammar.getProductionsForNonterminal(nonterminal):
-                        newProd = Production(nonterminal,["."] + p.strip().split(" "))
+                        newProd = Production(nonterminal, ["."] + p.strip().split(" "))
                         if newProd not in closure:
                             closure.append(newProd)
         return closure
@@ -59,7 +59,7 @@ class Parser:
     def goTo(self, state, x):
         gt = []
         for production in state:
-            if self.dot_before_symbol(production,x):
+            if self.dot_before_symbol(production, x):
                 newProd = self.move_dot(production)
                 c = self.closure(newProd)
                 for p in c:
@@ -67,7 +67,7 @@ class Parser:
                         gt.append(p)
         return gt
 
-    def dot_before_symbol(self,production,symbol):
+    def dot_before_symbol(self, production, symbol):
         dot_index = production.rhs.index(".")
         if dot_index + 1 == len(production.rhs):
             return False
@@ -83,6 +83,16 @@ class Parser:
             return True, production.rhs[dot_index + 1]
         return False, None
 
+    def dot_after_nonterminal(self, production):
+        dot_index = production.rhs.index(".")
+        if dot_index == 0:
+            return False, None
+        if production.rhs[dot_index - 1] in self.grammar.getNonterminals():
+            return True, production.rhs[dot_index - 1]
+        return False, None
+
     def move_dot(self, production):
         dot_index = production.rhs.index(".")
-        return Production(production.lhs, list(production.rhs[:dot_index]) + list(production.rhs[dot_index + 1]) + list(["."]) + list(production.rhs[dot_index+2:]))
+        return Production(production.lhs,
+                          list(production.rhs[:dot_index]) + list(production.rhs[dot_index + 1]) + list(["."]) + list(
+                              production.rhs[dot_index + 2:]))
---
+++ lab5/ParsingTable.py
@@ -0,0 +1,30 @@
+from Parser import Parser
+
+
+class ParsingTable:
+    def __init__(self, grammar):
+        self.grammar = grammar
+        self.parser = Parser(self.grammar)
+        self.canCol = self.parser.col_can_LR0()
+        i = 0
+        for state in self.canCol:
+            print("State " + str(i) + ": ")
+            for prod in state:
+                print(prod)
+            i = i+1
+        self.table = []
+
+    def get_actions(self):
+        for state in self.canCol:
+
+    def action(self,state):
+        for production in state:
+            cond, nonterminal = self.parser.dot_before_nonterminal(production)
+            if cond is True:
+                return "shift"
+            cond,nonterminal = self.parser.dot_after_nonterminal(production)
+            if nonterminal != "S\'":
+                return "reduce " + self.canCol.index(state)
+            else:
+                return "acc"
+        return "error"
